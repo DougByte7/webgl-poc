b44b681ce6b89697d01cfc082c8c31771908d5d7
Douglas Morriesen de Almeida <dougbyte7@gmail.com>
2021-05-09 20:23:35 -0300

    Add error btn

--- src/components/common/button/button-view.tsx
+++ src/components/common/button/button-view.tsx
@@ -1,21 +1,30 @@
 import { FunctionComponent } from "react"
-import './button.scss'
+import "./button.scss"
 
 interface ButtonProps {
   className: string
   title: string
+  type?: "default" | "error"
   children: React.ReactNode
   onClick: React.MouseEventHandler<HTMLButtonElement>
 }
 
 const Button: FunctionComponent<ButtonProps> = (props) => {
-  const { title, children, className, onClick } = props
+  const { title, children, className, type, onClick } = props
 
   return (
-    <button className={`button ${className}`} title={title} onClick={onClick}>
+    <button
+      className={`button ${type} ${className}`}
+      title={title}
+      onClick={onClick}
+    >
       {children}
     </button>
   )
+}
+
+Button.defaultProps = {
+  type: "default",
 }
 
 export default Button
